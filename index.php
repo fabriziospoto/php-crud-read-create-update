758c1555a59c2bdd7e311f44563eaad179711e31
Fabrizio <fabriziospoto1@gmail.com>
2020-09-29 17:40:10 +0200

    1.0
    1.0

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+include __DIR__ . '/partials/templates/header.php';
+include __DIR__ . '/partials/home/server.php';
+
+?>
+
+
+    <body>
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>ROOM NUMBER</th>
+                        <th>FLOOR</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($results as $room) { ?>
+                    <tr>
+                        <td> <?php echo $room['id']; ?></td>
+                        <td> <?php echo $room['room_number']; ?></td>
+                        <td> <?php echo $room['floor']; ?></td>
+                        <td> <a href="#">VIEW</a> </td>
+                        <td> <a href="#">UPDATE</a></td>
+                        <td> <a href="#">DELETE</a></td>
+                    </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
